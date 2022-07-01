feeb1df24ea9d51348be07f5f7eafcfc66f4103a
EailFly Bai <eailfly@gmail.com>
2022-07-01 17:13:36 +0800

    add smolck/command-completion.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -98,6 +98,8 @@ return packer.startup(function(use)
   -- hop
   use "phaazon/hop.nvim"
 
+  use "smolck/command-completion.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
